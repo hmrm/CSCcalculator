28be79526893cc1aa9d51f09c9a0b1559e7e4d44
hmrm <hmrmaxwell@gmail.com>
2013-03-15 13:54:51 -0500

    slider is now working

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,22 @@
-$("input").on("change", function(){ 
-	$("#yearsToComplete").text(Math.ceil(parseFloat($("#inputLoan").val()) / (parseFloat($("#inputRevenue").val()) * .05)))
-	    })
+var changeFx = function(loan, percent, revenue){ 
+    var months = (loan / ((revenue * percent) / (1200))).toFixed(0)
+    if (!isNaN(months)){
+	$("#monthsToComplete").text(months);
+    }
+};
+
+$("input").on("change", function(){ changeFx(parseInt($("#inputBorrow").val()), $("#slider").slider("value"), parseInt($("#inputRevenue").val()))});
 
 $(function () {
-    $("#slider").slider();
+    $("#slider").slider({
+	value: 2.5,
+	min: .5,
+	max: 5,
+	step: .5,
+	slide: function (event, ui) {
+	    $( "#percent" ).text( ui.value + "%");
+	    changeFx(parseInt($("#inputBorrow").val()), ui.value, parseInt($("#inputRevenue").val()));
+	}
+    });
+    $( "#percent" ).text( "" + $("#slider" ).slider("value") + "%");
 });
