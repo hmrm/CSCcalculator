31a5efbe958bc4ceef1ca0fe04a7fb014dec9d68
hmrm <hmrmaxwell@gmail.com>
2013-03-14 19:25:39 -0500

    working on the correct functionality and the slider

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,7 @@
 $("input").on("change", function(){ 
 	$("#yearsToComplete").text(Math.ceil(parseFloat($("#inputLoan").val()) / (parseFloat($("#inputRevenue").val()) * .05)))
 	    })
+
+$(function () {
+    $("#slider").slider();
+});
